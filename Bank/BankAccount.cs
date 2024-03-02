4f59531a73da6eb38bfcc698ceaf1d42272b9ef9
Lívia Bonotto <87043469+liviabonotto@users.noreply.github.com>
2024-03-01 22:57:54 -0300

    primeira versão do código - com teste dando errado

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -40,7 +40,7 @@ namespace BankAccountNS
                 throw new ArgumentOutOfRangeException("amount");
             }
 
-            m_balance += amount; // intentionally incorrect code
+            m_balance -= amount; //linha corrigida
         }
 
         public void Credit(double amount)
